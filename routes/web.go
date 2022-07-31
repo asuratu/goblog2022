a4f8b5dbd0709ce72df5765c315b1e7775cbf1e2
AsuraTu <tugecn@gmail.com>
2022-07-31 22:00:48 +0800

    解决循环引用问题

--- routes/web.go
+++ routes/web.go
@@ -17,4 +17,8 @@ func RegisterWebRoutes(r *mux.Router) {
 
 	// 自定义 404 页面
 	r.NotFoundHandler = http.HandlerFunc(pc.NotFound)
+
+	// 文章相关页面
+	ac := new(controllers.ArticlesController)
+	r.HandleFunc("/articles/{id:[0-9]+}", ac.Show).Methods("GET").Name("articles.show")
 }
