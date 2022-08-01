f830ed05a126fc862eabc70cf21cd01f344ca8d6
AsuraTu <tugecn@gmail.com>
2022-08-01 16:10:14 +0800

    重构中间件

--- main.go
+++ main.go
@@ -1,86 +1,17 @@
 package main
 
 import (
-	"database/sql"
-	"github.com/gorilla/mux"
 	"goblog/app/global"
+	"goblog/app/http/middlewares"
 	"goblog/bootstrap"
-	"goblog/pkg/database"
+	"goblog/pkg/logger"
 	"net/http"
-	"strconv"
-	"strings"
 )
 
-var db *sql.DB
-
-// Article  对应一条文章数据
-type Article struct {
-	Title, Body string
-	ID          int64
-}
-
-func forceHTMLMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 1. 设置标头
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		// 2. 继续处理请求
-		next.ServeHTTP(w, r)
-	})
-}
-
-// getArticleByID 获取文章
-func getArticleByID(id string) (Article, error) {
-	article := Article{}
-	query := "SELECT * FROM articles WHERE id = ?"
-	err := db.QueryRow(query, id).Scan(&article.ID, &article.Title, &article.Body)
-	return article, err
-}
-
-// getRouteVariable 获取 URI 路由参数
-func getRouteVariable(parameterName string, r *http.Request) string {
-	vars := mux.Vars(r)
-	return vars[parameterName]
-}
-
-func removeTrailingSlash(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 1. 除首页以外，移除所有请求路径后面的斜杆
-		if r.URL.Path != "/" {
-			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
-		}
-		// 2. 将请求传递下去
-		next.ServeHTTP(w, r)
-	})
-}
-
-// Delete 方法用以从数据库中删除单条记录
-func (a Article) Delete() (rowsAffected int64, err error) {
-	rs, err := db.Exec("DELETE FROM articles WHERE id = " + strconv.FormatInt(a.ID, 10))
-
-	if err != nil {
-		return 0, err
-	}
-
-	// √ 删除成功，跳转到文章详情页
-	if n, _ := rs.RowsAffected(); n > 0 {
-		return n, nil
-	}
-
-	return 0, nil
-}
-
 func main() {
-	database.Initialize()
-	db = database.DB
-
 	bootstrap.SetupDB()
 	bootstrap.InitRouter()
 
-	// 中间件：强制内容类型为 HTML
-	global.Router.Use(forceHTMLMiddleware)
-
-	err := http.ListenAndServe(":3000", removeTrailingSlash(global.Router))
-	if err != nil {
-		panic(err)
-	}
+	err := http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(global.Router))
+	logger.LogError(err)
 }
--- routes/web.go
+++ routes/web.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"goblog/app/http/controllers"
+	"goblog/app/http/middlewares"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -35,4 +36,7 @@ func RegisterWebRoutes(r *mux.Router) {
 
 	// 删除文章
 	r.HandleFunc("/articles/{id:[0-9]+}/delete", ac.Delete).Methods("POST").Name("articles.delete")
+
+	// 中间件：强制内容类型为 HTML
+	r.Use(middlewares.ForceHTML)
 }
