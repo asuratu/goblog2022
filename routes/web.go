ba6b8d932cf5747589b07b7df7fdd9518aeabef7
TG <asura@Antony.local>
2022-08-01 16:32:35 +0800

    美化文章列表页

--- routes/web.go
+++ routes/web.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"goblog/app/http/controllers"
-	"goblog/app/http/middlewares"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -10,6 +9,10 @@ import (
 
 // RegisterWebRoutes 注册网页相关路由
 func RegisterWebRoutes(r *mux.Router) {
+
+	// 静态资源
+	r.PathPrefix("/css/").Handler(http.FileServer(http.Dir("./public")))
+	r.PathPrefix("/js/").Handler(http.FileServer(http.Dir("./public")))
 
 	// 静态页面
 	pc := controllers.PagesController{}
@@ -38,5 +41,5 @@ func RegisterWebRoutes(r *mux.Router) {
 	r.HandleFunc("/articles/{id:[0-9]+}/delete", ac.Delete).Methods("POST").Name("articles.delete")
 
 	// 中间件：强制内容类型为 HTML
-	r.Use(middlewares.ForceHTML)
+	//r.Use(middlewares.ForceHTML)
 }
